d947a30aee76bccbbee3ce5846ae88a242104fd7
Marlisa Odokara <marlisa.od@gmail.com>
2024-01-08 22:37:18 -0600

    all about the properties file

--- src/main/java/com/example/demo/customer/CustomerConfiguration.java
+++ src/main/java/com/example/demo/customer/CustomerConfiguration.java
@@ -1,9 +1,13 @@
 package com.example.demo.customer;
 
+import com.example.demo.infoapp.InfoApp;
+import org.springframework.aop.scope.ScopedProxyUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 import java.util.List;
 
@@ -12,12 +16,15 @@ public class CustomerConfiguration {
 
     @Value("${app.useFakeCustomerRepo:false}")
 
+
+
     private Boolean useFakeCustomerRepo;
     @Bean
         //tell bean this needs to be initialized
-    CommandLineRunner commandLineRunner() {
+    CommandLineRunner commandLineRunner(InfoApp infoApp) {
         return args -> {
             System.out.println("Command Line Runner hooray");
+            System.out.println(infoApp);
         };
     }
 
